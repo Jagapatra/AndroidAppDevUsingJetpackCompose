912b9040328da35301f784acdb9ee8c3b70f3f59
Jagannath patra <127885365+Jagapatra@users.noreply.github.com>
2024-07-04 01:58:10 +0530

    Added text stling programs with screen shots

---
+++ Font 30/font.kt
@@ -0,0 +1,32 @@
+package com.example.sampleapplication1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.sampleapplication1.ui.theme.SampleApplication1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //enableEdgeToEdge()
+        setContent {
+            SampleApplication1Theme {
+                Text(text="Hello silicon",fontSize = 30.sp)
+            }
+        }
+    }
+}
+
+
---
+++ Gradient Text/gradient.kt
@@ -0,0 +1,49 @@
+package com.example.sampleapplication1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Blue
+import androidx.compose.ui.graphics.Color.Companion.Cyan
+import androidx.compose.ui.graphics.Color.Companion.Yellow
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.sampleapplication1.ui.theme.SampleApplication1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //enableEdgeToEdge()
+        setContent {
+            SampleApplication1Theme {
+
+                val gradientColors = listOf(Cyan, Yellow, Blue /*...*/)
+
+                Text(
+                    text = "Hello" ,
+                    style = TextStyle(
+                        fontSize = 50.sp,
+                        brush = Brush.linearGradient(
+                            colors = gradientColors
+
+                        )
+                    )
+                )
+            }
+        }
+    }
+}
+
+
