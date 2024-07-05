4fe04b60769b3bef8e2197533839d30f865187aa
Jagannath patra <127885365+Jagapatra@users.noreply.github.com>
2024-07-05 12:54:18 +0530

    Added rows files using nested rows and columns for testing

---
+++ Rows/RowColumnModifiers2.kt
@@ -0,0 +1,87 @@
+package com.example.sampleapplication1
+
+import android.os.Bundle
+import android.text.Layout
+import android.text.style.BackgroundColorSpan
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.basicMarquee
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Black
+import androidx.compose.ui.graphics.Color.Companion.Blue
+import androidx.compose.ui.graphics.Color.Companion.Cyan
+import androidx.compose.ui.graphics.Color.Companion.Gray
+import androidx.compose.ui.graphics.Color.Companion.LightGray
+import androidx.compose.ui.graphics.Color.Companion.Yellow
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.sampleapplication1.ui.theme.SampleApplication1Theme
+
+class MainActivity : ComponentActivity() {
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //enableEdgeToEdge()
+        setContent {
+            SampleApplication1Theme {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(10.dp)
+                        .background(Color.Blue),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.SpaceAround
+                ) {
+                    Row (
+                        modifier = Modifier
+
+                            .fillMaxWidth()
+                            .padding(10.dp)
+                            .background(Color.Green)
+                    ){
+                        Text(text = "This is", color = Color.Black, fontSize = 40.sp)
+                    }
+
+
+
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(10.dp)
+                            .background(Color.Gray),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceAround
+                    )
+                    {
+                        Text(text = "Batch 1", fontSize = 20.sp)
+                        Text("Batch 2 ", color = Color.Blue, fontSize = 20.sp)
+                        Text("Batch 3", color = Color.Cyan, fontSize = 20.sp)
+                        Text("Batch 4 ", color = Color.White, fontSize = 20.sp)
+                    }
+                }
+            }
+        }
+    }
+}
+
+
