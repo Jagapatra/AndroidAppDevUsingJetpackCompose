0125c4f6fc175a58ae86e76c6a9b28147c63bc8a
Jagannath patra <127885365+Jagapatra@users.noreply.github.com>
2024-07-10 14:07:38 +0530

    Added screen shot of mvvm implementation

--- Mvvm.kt
+++ Mvvm.kt
@@ -1,38 +1,175 @@
+package com.example.sampleapplication1
+
+import android.graphics.drawable.*
+import android.os.Bundle
+import android.text.Layout
+import android.text.style.BackgroundColorSpan
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.basicMarquee
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.AssistChip
+import androidx.compose.material3.AssistChipDefaults
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ChipColors
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ElevatedAssistChip
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ElevatedSuggestionChip
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.FilledIconButton
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.FilterChipDefaults
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButtonColors
+import androidx.compose.material3.InputChip
+import androidx.compose.material3.InputChipDefaults
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RangeSlider
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Shapes
+import androidx.compose.material3.Slider
+import androidx.compose.material3.SliderDefaults
+import androidx.compose.material3.SuggestionChip
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.VerticalDivider
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.*
+import androidx.compose.ui.graphics.Color.Companion.Black
+import androidx.compose.ui.graphics.Color.Companion.Blue
+import androidx.compose.ui.graphics.Color.Companion.Cyan
+import androidx.compose.ui.graphics.Color.Companion.DarkGray
+import androidx.compose.ui.graphics.Color.Companion.Gray
+import androidx.compose.ui.graphics.Color.Companion.LightGray
+import androidx.compose.ui.graphics.Color.Companion.Magenta
+import androidx.compose.ui.graphics.Color.Companion.Transparent
+import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.graphics.Color.Companion.Yellow
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.sampleapplication1.ui.theme.SampleApplication1Theme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+class MainActivity : ComponentActivity() {
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //enableEdgeToEdge()
+        setContent {
+            Column(
+                Modifier
+                    .fillMaxSize()
+                    .background(Color.Cyan),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceEvenly
+            ){
+                Text(text = "MVVM architecture \n" ,
+                    fontSize = 50.sp, textAlign = TextAlign.Center , )
+                CounterView()
+            }
+        }
+    }
+}
+//View MVVM
 @Composable
-fun Counter(counterViewModel: CounterViewModel = viewModel()) {
-    val count by counterViewModel.count.observeAsState(0)
-    Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(text = "Count is $count")
-        Button(onClick = { counterViewModel.increment() }) {
-            Text(text = "Increment")
+fun CounterView(counterVM: CounterViewModel = viewModel()){
+    val counterState=counterVM.counter.value
+    Column(horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center){
+
+        Text(text="Current Counter value: ${counterState.count}" , fontSize = 30.sp)
+        Row{
+            Button(onClick = {counterVM.increment()}){
+                Text("Increment")
+            }
+            Spacer(modifier = Modifier.width(10.dp))
+            Button(onClick = {counterVM.decrement()}){
+                Text("Decrement")
+            }
         }
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = { counterViewModel.decrement() }) {
-            Text(text = "Decrement")
-        }
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = { counterViewModel.reset() }) {
-            Text(text = "Reset")
+        Button(onClick = {counterVM.resetValue()}){
+            Text("Reset")
         }
     }
 }
-data class Counter(val count: Int)
 
-class CounterViewModel : ViewModel() {
-    private val _counter = mutableStateOf(Counter(count = 0))
+data class Counter(val count:Int)
+//data class User(val username:String, val password:String)
 
+class CounterViewModel: ViewModel(){
+    private val _counter= mutableStateOf(Counter(0))
     val counter: State<Counter> = _counter
-
-    fun increment() {
-        _counter.value = Counter(count = _counter.value.count + 1)
+    fun increment(){
+        _counter.value = Counter(_counter.value.count+1)
     }
-
-    fun decrement() {
-        _counter.value = Counter(count = _counter.value.count - 1)
+    fun decrement(){
+        _counter.value=Counter(_counter.value.count -1)
     }
-
-    fun reset() {
-        counter.value = Counter(count = 0)
+    fun resetValue(){
+        _counter.value=Counter(0)
     }
 }
-
